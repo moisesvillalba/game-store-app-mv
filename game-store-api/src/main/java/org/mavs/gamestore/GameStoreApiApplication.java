e525a1126e1d2b7fd6ac10bba2ad0f70c175cd96
Moises Villalba Silvero <moisesvillalba@pol.una.py>
2024-12-19 17:54:44 -0300

    cambios agregados desde intellij

---
+++ game-store-api/src/main/java/org/mavs/gamestore/GameStoreApiApplication.java
@@ -0,0 +1,13 @@
+package org.mavs.gamestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameStoreApiApplication.class, args);
+    }
+
+}
---
+++ game-store-api/src/test/java/org/mavs/gamestore/GameStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mavs.gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
